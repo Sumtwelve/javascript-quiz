8447c7e631fcf9c6ebd9a46a24a043db6274edec
sumtwelve <sumtwelve@gmail.com>
2023-01-03 00:41:13 -0700

    add 3 questions, begin work on displaying questions, began work on implementing and explaining array shuffle algo

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,27 @@ var quizTimer = document.getElementById("timer"); //span
 var titleText = document.getElementById("title-text");
 var descText = document.getElementById("desc-text");
 
-// 
-
+console.log(Math.floor(Math.random() * 5));
 
 // questions
 var questions = [
     {
-        question: "The \"+\" operator joins multiple values into one ________",
+        questionText: "The \"+\" operator joins multiple values into one ________",
         options: ["array", "string", "integer", "regular expression"],
         answer: "string"
     }, {
-        question: "How do you stop the repeated execution of code in a <code>setInterval()</code> function?",
-        options: ["<code>break;</code>", "<code>stopTimer();</code>", "<code>clearInterval()</code>", "<code>timer.remove();</code>"]
+        questionText: "How do you stop the repeated execution of code in a <code>setInterval()</code> function?",
+        options: ["<code>break;</code>", "<code>stopTimer();</code>", "<code>clearInterval()</code>", "<code>timer.remove();</code>"],
+        answer: "<code>clearInterval()</code>"
     }, {
-        question: ""
+        questionText: "The variables between parentheses in a function definition are ________ , and when calling a function, you pass in matching values as ________",
+        options: ["parameters, arguments", "variables, values", "attributes, arguments", "properties, values"],
+        answer: "parameters, arguments"
     }
 ];
 
 
+// add event listener to landing page start button and begin the quiz!!
 startBtn.addEventListener("click", startQuiz);
 
 function startQuiz(event) {
@@ -39,11 +42,11 @@ function startQuiz(event) {
     timer.textContent = "05:00";
     startTimer(0,3, playerLoses);
 
+    // display question
+    cycleQuestions();
+
     // change text boxes to the first or next question, and restyle appropriately to make it look less like the quiz landing page
-    titleText.textContent = "wow";
-    titleText.setAttribute("style", "text-align: left; margin-left: 25vw;");
-    descText.textContent = "";
-    descText.setAttribute("style", "text-align: left; margin-left: 25vw; padding-right: 20vw;")
+    
 
 }
 
@@ -64,10 +67,32 @@ function startTimer(minutes, seconds, doWhenOver) {
     }, 1000);
 }
 
+
+function cycleQuestions() {
+    // restyle textboxes to look less like quiz landing page
+    titleText.setAttribute("style", "text-align: left; margin-left: 25vw;");
+    descText.setAttribute("style", "text-align: left; margin-left: 25vw; padding-right: 20vw;")
+    descText.textContent = "";
+
+    // display the questions
+    var question = questions[0];
+    titleText.textContent = question.questionText;
+}
+
+
 function playerLoses() {
     console.log("time is up!! player lost!!");
     titleText.textContent = "Time's up!";
 }
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// ANCILLARY FUNCTIONS BELOW /////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 
 // toMMSS() takes an integer seconds and converts it to a string in MM:SS format
 function toMMSS(seconds) {
@@ -91,16 +116,29 @@ function toMMSS(seconds) {
     return extraZeroM + minutes + ":" + extraZeroS + remainingSeconds;
 }
 
-// toSeconds() takes a number of type unit and returns its value in seconds
-// startTimer function only takes seconds as argument, so I created this function
-// to allow you to pass
-function toSeconds(unit, value) {
-    switch (unit) {
-        case "hour":
-            return value * 3600;
-        case "minute":
-            return value * 60;
-        default:
-            return null;
+
+// FISHER-YATES ARRAY SHUFFLE
+// Credit for this code goes to Mike Bostock, https://bost.ocks.org/mike/shuffle/
+// This algorithm shuffles the array in place by choosing a random index from the front of the array
+// and sending it to the back of the array.
+// The 'front' and 'back' are delimited by m, a number that decreases by 1 per random choosing,
+// thus it acts as a current index marker. All random indexes chosen will be less than m, so no element
+// can be selected twice.
+// 
+function shuffle(array) {
+    // initialize m (total number of elements), plus undef variables t and i
+    var m = array.length, t, i;
+
+    // shuffle will cease when m == 0
+    while (m) {
+        // pick a random element that has not been picked yet
+        i = Math.floor(Math.random() * m--);
+
+        // temporarily store final 'front' element
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
     }
+
+    return array;
 }
